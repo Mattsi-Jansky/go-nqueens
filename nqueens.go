7e629d034086b7f392fbdda78d0cf4ecd9283042
Mattsi <mattsi.jansky@codurance.com>
2024-11-03 20:19:21 +0000

    Basic algorithm structure

--- nqueens.go
+++ nqueens.go
@@ -1,7 +1,22 @@
 package nqueens
 
 func Nqueens(size int) [][]int {
-	return [][]int{
-		{},
+	frontier := make([][]int, 0)
+	results := make([][]int, 0)
+	for i := 0; i < size; i++ {
+		frontier = append(frontier, []int{i})
 	}
+
+	for len(frontier) > 0 {
+		board := frontier[0]
+		frontier = frontier[1:]
+
+		//  If the board is not valid, continue
+		if len(board) == size {
+			results = append(results, board)
+		}
+		//  If the board is incomplete, add next possible states to frontier
+	}
+
+	return results
 }
